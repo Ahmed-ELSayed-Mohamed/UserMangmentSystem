4503bf4495a31dfed34ac8e06e95ecabc8aa24fb
AhmedELsayedMohamed <ahmed771994@gmail.com>
2023-02-16 22:19:59 +0200

    Configure Serivce for IEmployee Repo

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserMangmentSystem.Model;
 
 namespace UserMangmentSystem
 {
@@ -16,6 +17,8 @@ namespace UserMangmentSystem
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IEmployeeRepo,MockEmployee>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +33,11 @@ namespace UserMangmentSystem
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
         }
     }
